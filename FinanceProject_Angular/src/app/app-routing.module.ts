6a1093d42ea06e4113f83921c3c8dc3a9eaac731
Keegers <kmr025@email.uark.edu>
2020-03-22 21:28:47 -0500

    Added routeing to login page as well as login page html

--- FinanceProject_Angular/src/app/app-routing.module.ts
+++ FinanceProject_Angular/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginGuardService } from './authoguards/login/login-guard.service';
+import { Auth1Service } from './auth_services/auth1.service';
+import { LoginComponent } from './login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full', canActivate: [Auth1Service]},
+  { path: 'login', component: LoginComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ FinanceProject_Angular/src/app/auth_services/auth1.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { LoginGuardService } from '../authoguards/login/login-guard.service';
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Auth1Service {
+
+  constructor(private router: Router, private loginService: LoginGuardService) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (this.loginService.isUserLoggedIn) {
+      return true;
+    }
+    this.router.navigate(['login']);
+    return false;
+  }
+}
